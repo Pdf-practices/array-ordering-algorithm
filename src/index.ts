e2eaf19dbe2d5f4f929b9d9de6d212167ca72036
wdavidcalsin <david2c142@gmail.con>
2021-01-13 22:48:56 -0500

    refactor: 💡 merge sort was modified

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import OrderByBubbles from "./bubbles/index";
 import OrderingByInseercion from "./insertion";
+import mergeSort from "./merge";
 
 function main() {
-   /**
-    * Sorting with the bubble algorithm
-    */
-   //    let orderBubble = OrderByBubbles([89, 45, 79, 47, 23, 50, 200, 1, 6]);
-   //    console.log(orderBubble);
+  /**
+   * Sorting with the bubble algorithm
+   */
+  //    let orderBubble = OrderByBubbles([89, 45, 79, 47, 23, 50, 200, 1, 6]);
+  //    console.log(orderBubble);
 
-   /**
-    * Sorting with the insertion algorithm
-    */
-   let orderInsertion = OrderingByInseercion([45, 5, 2, 0, 23, 4]);
-   console.log(orderInsertion);
+  /**
+   * Sorting with the insertion algorithm
+   */
+  // let orderInsertion = OrderingByInseercion([45, 5, 2, 0, 23, 4]);
+  // console.log(orderInsertion);
+
+  /**
+   * Sorting with the merge algorithm
+   */
+  const arr = [45, 5, 2, 0, 23, 4, 54, 76, 3, 293, 100, 41];
+
+  let MergeSort = mergeSort(arr);
+  console.log(MergeSort);
 }
 
 main();

26e28ec9cd9995c3ae29f574b7ad517ebc8d38a4
WDavid Calsin <david2c142@gmail.com>
2021-01-14 21:01:46 -0500

    Travis cl config 2 (#8)
    * test: 💍 test for selection sort
    
    automation and configuration with travis is being added, test algorithms
    are being added
    
    * test: 💍 command npm install fixed
    
    Co-authored-by: wdavidcalsin <david2c142@gmail.con>

--- src/selection/index.ts
+++ src/selection/index.ts
@@ -1,4 +1,4 @@
-const selectionSort = (arr: Array<number>) => {
+const selectionSort = (arr: Array<number> = [12, 45, 1, 890]) => {
   for (let i = 0; i < arr.length; ++i) {
     let j = i;
     let jMin = i;
